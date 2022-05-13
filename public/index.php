dab0d85ff5da4f17168724441775f32376ba509d
hdm <hdm58@qq.com>
2022-05-13 17:48:18 +0800

    优化描述

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @copyright Copyright (c) 2021 勾股工作室
- * @license https://opensource.org/licenses/GPL-2.0
+ * @copyright Copyright (c) 2022 勾股工作室
+ * @license https://opensource.org/licenses/GPL-3.0
  * @link https://www.gougucms.com
  */
  
@@ -15,7 +15,7 @@ if (empty(file_exists(__DIR__ . '/../vendor/autoload.php'))) {
 require __DIR__ . '/../vendor/autoload.php';
 
 // 定义当前版本号
-define('CMS_VERSION','1.4.18');
+define('CMS_VERSION','2.5.15');
 
 // 定义Layui版本号
 define('LAYUI_VERSION','2.6.8');
